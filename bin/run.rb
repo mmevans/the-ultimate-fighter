c91446e298b79a6ae414e30b42bc24d1ce000d22
BarretteBanner <barrettebanner@gmail.com>
2019-08-19 14:00:41 -0500

    starting tty

--- bin/run.rb
+++ bin/run.rb
@@ -1,11 +1,14 @@
-require_relative '../config/environment'
+require_relative "../config/environment"
 require "tty-prompt"
 
 prompt = TTY::Prompt.new
 
 
-puts "hello world"
-puts "Hey, it's Barrette!"
-puts "hello world"
-puts "Hey, it's Michael!"
+# puts "hello world"
+# puts "Hey, it's Barrette!"
+# puts "hello world"
+# puts "Hey, it's Michael!"
 
+prompt.say("Hello! Welcome to The Ultimate Fighter!")
+
+prompt.select("Choose your destiny?", ["Sign", "Login"])
